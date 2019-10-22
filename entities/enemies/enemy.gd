38970c109e0866f2c4d764d1e8ef3377b6d1ee36
Ronan Duffy <D00181131@student.dkit.ie>
2019-10-22 20:41:31 +0100

    - There is now a base attack state for the Enemy scene. 	It's expected to be overriden. - Added a background to the game. - Target updating is now a function in the Enemy scene. - Same for target offset updating.

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -1,14 +1,22 @@
 extends KinematicBody2D
+"""
+This represents a character trying to encounter the Skunk Ape.
+Their behaviour might vary, but generally they'll try to move to a location around Skongk,
+attempting an attack every interval.
+"""
 
 enum States {GO_TO_TARGET, IDLE, ATTACK}
 
 export (float) var move_speed : float = 60.0
+export (Vector2) var base_target_offset : Vector2
+export (Vector2) var offset_variance : Vector2
+export (bool) var face_target : bool = true
 
 var current_state : int = States.GO_TO_TARGET
 
 var _skongk_ref : KinematicBody2D
 var _target : Vector2
-var _target_y_offset : float = 0.0
+var _target_offset : Vector2
 var _target_updates : int = 0
 
 func _ready() -> void:
@@ -16,9 +24,9 @@ func _ready() -> void:
 	if !_skongk_ref:
 		_target = position
 		$TargetUpdater.stop()
-	
-	randomize()
-	_target_y_offset = rand_range(-32.0, 32)
+	else:
+		_update_target_offset()
+	$AttackTimer.start()
 
 func _draw() -> void:
 	draw_circle(to_local(_target), 5, Color.yellow)
@@ -39,15 +47,39 @@ func _physics_process(delta : float) -> void:
 			if position.distance_to(_target) > 32.0:
 				current_state = States.GO_TO_TARGET
 
+func _update_target() -> void:
+	var new_offset = _target_offset
+	if face_target:
+		new_offset.x *= _skongk_ref.current_direction
+	
+	_target = _skongk_ref.position + new_offset
+
+func _update_target_offset() -> void:
+	_target_offset = base_target_offset
+	_target_offset.x += rand_range(-offset_variance.x, offset_variance.x)
+	_target_offset.y += rand_range(-offset_variance.y, offset_variance.y)
+
 func _get_skongk_reference() -> void:
 	var skongk_group = get_tree().get_nodes_in_group("skongk")
 	if !skongk_group.empty():
 		_skongk_ref = skongk_group[0]
 
 func _on_TargetUpdater_timeout():
-	_target = _skongk_ref.position + Vector2(32 * _skongk_ref.current_direction, _target_y_offset)
+	_update_target()
 	if _target_updates >= 4:
-		_target_y_offset = rand_range(-32.0, 32)
+		_update_target_offset()
 		_target_updates = 0
 	else:
 		_target_updates += 1
+
+func _on_AttackTimer_timeout():
+	current_state = States.ATTACK
+	$TargetUpdater.stop()
+	$AnimationPlayer.play("attack")
+	print("Enemy is attacking")
+
+func _on_AnimationPlayer_animation_finished(anim_name : String) -> void:
+	if anim_name == "attack":
+		current_state = States.IDLE
+		$TargetUpdater.start()
+		$AttackTimer.start()
